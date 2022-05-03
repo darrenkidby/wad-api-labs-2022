580dced3d39cddc46c47c414835a16f9765e4b61
Darren Kidby (20088411) <20088411@wit.ie>
2022-05-03 14:15:39 +0100

    Topic 11 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,3 +1,4 @@
+import { getUpcomingMovies } from '../tmdb-api';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
@@ -52,5 +53,10 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 export default router;
