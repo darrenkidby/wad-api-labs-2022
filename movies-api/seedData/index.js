9ae17245724b4d6eee2dc6641c65b9a6a23e194d
Darren Kidby (20088411) <20088411@wit.ie>
2022-05-03 11:51:08 +0100

    Topic 10 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,10 +1,11 @@
 import userModel from '../api/users/userModel';
 import users from './users';
 import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 dotenv.config();
 
-// deletes all user documents in collection and inserts test data
 // deletes all user documents in collection and inserts test data
 async function loadUsers() {
   console.log('load user Data');
@@ -17,6 +18,20 @@ async function loadUsers() {
   }
 }
 
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
+
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();//ADD THIS LINE
 }
