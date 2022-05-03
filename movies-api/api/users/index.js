0d49bf7138268ef8b5627eb2939aed8d6e494906
Darren Kidby (20088411) <20088411@wit.ie>
2022-05-03 10:05:26 +0100

    Mongo lab User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -9,6 +9,15 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
+
 // register(Create)/Authenticate User
 router.post('/', async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
@@ -28,16 +37,30 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Update a user
-router.put('/:id', async (req, res) => {
-    if (req.body._id) delete req.body._id;
-    const result = await User.updateOne({
-        _id: req.params.id,
-    }, req.body);
-    if (result.matchedCount) {
-        res.status(200).json({ code:200, msg: 'User Updated Sucessfully' });
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to Update User' });
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
     }
 });
+
+// Update a user
+  router.put('/:id', async (req, res) => {
+      if (req.body._id) delete req.body._id;
+      const result = await User.updateOne({
+          _id: req.params.id,
+      }, req.body);
+      if (result.matchedCount) {
+          res.status(200).json({ code:200, msg: 'User Updated Sucessfully' });
+      } else {
+          res.status(404).json({ code: 404, msg: 'Unable to Update User' });
+      }
+  });
 export default router;
